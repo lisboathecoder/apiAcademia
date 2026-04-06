2cd9d0535040e81da492e2226a66d9f14da22e0d
Gustavo Lisboa <155676204+lisboathecoder@users.noreply.github.com>
2026-04-06 10:58:45 -0300

    Delete src/routes/treinoPdfRoute.js

--- src/routes/treinoPdfRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as controller from '../controllers/pdfController.js';
-
-const router = express.Router();
-
-router.get('/pdf', controller.buscarTodosTreinos);
-router.get('/:id/pdf', controller.relatorioTreinoPorId);
-
-export default router;
